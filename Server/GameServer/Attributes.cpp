28d28dfc2969e37707c68437f6f485ed4371315e
TolisM <Kadaz@users.noreply.github.com>
2019-08-16 23:41:06 +0300

    Add files via upload
    Testing Attributes

--- Server/GameServer/Attributes.cpp
+++ Server/GameServer/Attributes.cpp
@@ -30,71 +30,66 @@ void PlayerAttributes::LoadAttributesFromDB(int charID)
 	sPC_TBLDAT *pTblData;
 	//Load All Attributes One time only - Luiz  IN ORDER --Kalisto
 	//STR 
-	this->sPlayerAttribute.byBaseStr = 0;// db->getInt("BaseStr") + db->getInt("Level")*0.2;
-	this->sPlayerAttribute.byLastStr = 14 * 1 + db->getInt("Level")*0.4;
+	this->sPlayerAttribute.byBaseStr = db->getInt("BaseStr") + db->getInt("Level")*0.2;
+	this->sPlayerAttribute.byLastStr = db->getInt("LastStr") + db->getInt("Level")*0.4;
 	//Constitucion
-	this->sPlayerAttribute.byBaseCon = 0;// db->getInt("BaseCon") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.byLastCon = 9 * 1 + db->getInt("Level")*0.4;
+	this->sPlayerAttribute.byBaseCon = db->getInt("BaseCon") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.byLastCon = db->getInt("LAstCon") + db->getInt("Level")*0.4;
 	//Focus
-	this->sPlayerAttribute.byBaseFoc = 0;// db->getInt("BaseFoc") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.byLastFoc = 9 * 1 + db->getInt("Level")*0.4;
+	this->sPlayerAttribute.byBaseFoc = db->getInt("BaseFoc") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.byLastFoc = db->getInt("LastFoc") + db->getInt("Level")*0.4;
 	//Dextry
-	this->sPlayerAttribute.byBaseDex = 0;// db->getInt("BaseDex") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.byLastDex = 14 * 1 + db->getInt("Level")*0.4;
+	this->sPlayerAttribute.byBaseDex = db->getInt("BaseDex") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.byLastDex = db->getInt("LastDex") + db->getInt("Level")*0.4;
 	//Soul
-	this->sPlayerAttribute.byBaseSol = 0;// *db->getInt("Level")*0.2;
-	this->sPlayerAttribute.byLastSol = 9 * 1 + db->getInt("Level")*0.4;
+	this->sPlayerAttribute.byBaseSol = db->getInt("BaseSol") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.byLastSol = db->getInt("LastSol") + db->getInt("Level")*0.4;
 	//Energy
-	this->sPlayerAttribute.byBaseEng = 0;// db->getInt("BaseEng") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.byLastEng = 7 * 1 + db->getInt("Level")*0.4;	
+	this->sPlayerAttribute.byBaseEng = db->getInt("BaseEng") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.byLastEng = db->getInt("LastEng") + db->getInt("Level")*0.4;	
 	//EP/LP
-	this->sPlayerAttribute.wBaseMaxLP = 9000;
-	this->sPlayerAttribute.wLastMaxLP = 9000;
-	this->sPlayerAttribute.wBaseMaxEP = 9000;
-	this->sPlayerAttribute.wLastMaxEP = 9000;
+	this->sPlayerAttribute.wBaseMaxLP = db->getInt("BaseMaxLP");
+	this->sPlayerAttribute.wLastMaxLP = db->getInt("LastMaxLP");
+	this->sPlayerAttribute.wBaseMaxEP = db->getInt("BaseMaxEP");
+	this->sPlayerAttribute.wLastMaxEP = db->getInt("LastMaxEP");
 	//Physical Atack
-	this->sPlayerAttribute.wBasePhysicalOffence = 0;// db->getInt("BasePhysicalOffence") * db->getInt("Level")*1.5;
-	this->sPlayerAttribute.wLastPhysicalOffence = 28 + this->sPlayerAttribute.byLastStr * db->getInt("Level")*0.10;
+	this->sPlayerAttribute.wBasePhysicalOffence = db->getInt("BasePhysicalOffence") * db->getInt("Level")*1.5;
+	this->sPlayerAttribute.wLastPhysicalOffence = db->getInt("LastPhysicalOffence") * db->getInt("Level")*0.10;
 	//Physical Defese
-	this->sPlayerAttribute.wBasePhysicalDefence = 0;// db->getInt("BasePhysicalDefence") * db->getInt("Level")*1.2;
-	this->sPlayerAttribute.wLastPhysicalDefence = 0 *db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wBasePhysicalDefence = db->getInt("BasePhysicalDefence") * db->getInt("Level")*1.2;
+	this->sPlayerAttribute.wLastPhysicalDefence = db->getInt("LastPhysicalDefence") *db->getInt("Level")*0.2;
 	//Energy Atack
-	this->sPlayerAttribute.wBaseEnergyOffence = 0;// db->getInt("BaseEnergyOffence") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.wLastEnergyOffence = 15 + this->sPlayerAttribute.byLastSol * db->getInt("Level")*0.07;
+	this->sPlayerAttribute.wBaseEnergyOffence = db->getInt("BaseEnergyOffence") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wLastEnergyOffence = db->getInt("LastEnergyOffence") * db->getInt("Level")*0.07;
 	//Energy Defese
-	this->sPlayerAttribute.wBaseEnergyDefence = 0;// db->getInt("BaseEnergyDefence") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.wLastEnergyDefence = 0 * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wBaseEnergyDefence = db->getInt("BaseEnergyDefence") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wLastEnergyDefence = db->getInt("LastEnergyDefence") * db->getInt("Level")*0.2;
 	//Hit Rate
-	this->sPlayerAttribute.wBaseAttackRate = 0;// db->getInt("BaseAttackRate") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.wLastAttackRate = 70 + this->sPlayerAttribute.byLastFoc * db->getInt("Level")*0.3;
+	this->sPlayerAttribute.wBaseAttackRate = db->getInt("BaseAttackRate") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wLastAttackRate = db->getInt("LastAttackRate") * db->getInt("Level")*0.3;
 	//Dogge Rate
-	this->sPlayerAttribute.wBaseDodgeRate = 0;// db->getInt("BaseDodgeRate") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.wLastDodgeRate = 90 + this->sPlayerAttribute.byLastDex * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wBaseDodgeRate = db->getInt("BaseDodgeRate") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wLastDodgeRate = db->getInt("LastDodgeRate") * db->getInt("Level")*0.2;
 	//Physical Critical Rate
-	this->sPlayerAttribute.wBasePhysicalCriticalRate = 0;// db->getInt("BasePhysicalCriticalRate") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.wLastPhysicalCriticalRate = 7 + this->sPlayerAttribute.byLastDex * db->getInt("Level")*0.012;
+	this->sPlayerAttribute.wBasePhysicalCriticalRate = db->getInt("BasePhysicalCriticalRate") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wLastPhysicalCriticalRate = db->getInt("LastPhysicalCriticalRate") * db->getInt("Level")*0.012;
 	//Energy Critical Rate
-	this->sPlayerAttribute.wBaseEnergyCriticalRate = 0;// db->getInt("BaseEnergyCriticalRate") * db->getInt("Level")*0.2;
-	this->sPlayerAttribute.wLastEnergyCriticalRate = 6 + this->sPlayerAttribute.byLastFoc * db->getInt("Level")*0.01;
+	this->sPlayerAttribute.wBaseEnergyCriticalRate = db->getInt("BaseEnergyCriticalRate") * db->getInt("Level")*0.2;
+	this->sPlayerAttribute.wLastEnergyCriticalRate = db->getInt("LastEnergyCriticalRate") * db->getInt("Level")*0.01;
 	//RP 
-	this->sPlayerAttribute.wBaseMaxRP = 0;// db->getInt("BaseMaxRP");
-	this->sPlayerAttribute.wLastMaxRP = 0;// db->getInt("LastMaxRP");
+	this->sPlayerAttribute.wBaseMaxRP = db->getInt("BaseMaxRP");
+	this->sPlayerAttribute.wLastMaxRP = db->getInt("LastMaxRP");
 	//Block Rate 
-	this->sPlayerAttribute.wBaseBlockRate = 0;//db->getInt("BaseBlockRate");
-	this->sPlayerAttribute.wLastBlockRate = 0;// db->getInt("LastBlockRate");
+	this->sPlayerAttribute.wBaseBlockRate = db->getInt("BaseBlockRate");
+	this->sPlayerAttribute.wLastBlockRate = db->getInt("LastBlockRate");
 	//Run Speed
-	this->sPlayerAttribute.fLastRunSpeed = 10;// (float)db->getDouble("LastRunSpeed");
+	this->sPlayerAttribute.fLastRunSpeed = (float)db->getDouble("LastRunSpeed");
 	//Atack Speed
-	this->sPlayerAttribute.wBaseAttackSpeedRate = 1000;// (float)db->getInt("BaseAttackSpeedRate");
-	this->sPlayerAttribute.wLastAttackSpeedRate = 1000;//(float)db->getInt("LastAttackSpeedRate");
+	this->sPlayerAttribute.wBaseAttackSpeedRate = (float)db->getInt("BaseAttackSpeedRate");
+	this->sPlayerAttribute.wLastAttackSpeedRate = (float)db->getInt("LastAttackSpeedRate");
 	//Atack Range
-	this->sPlayerAttribute.fLastAttackRange = 0;// (float)db->getInt("LastAttackRange");
-	this->sPlayerAttribute.fBaseAttackRange = 0;// (float)db->getInt("BaseAttackRange");
-	//nao sei....
-	this->sPlayerAttribute.unknown_int16[0] = 0;
-	this->sPlayerAttribute.unknown_int16[1] = 0;
-	this->sPlayerAttribute.unknown_int16[2] = 0;
-	this->sPlayerAttribute.unknown_int16[3] = 0;
+	this->sPlayerAttribute.fLastAttackRange = (float)db->getInt("LastAttackRange");
+	this->sPlayerAttribute.fBaseAttackRange = (float)db->getInt("BaseAttackRange");
 	//LP Get up Reg
 	this->sPlayerAttribute.wBaseLpRegen = 20;
 	this->sPlayerAttribute.wLastLpRegen = 20;
@@ -114,76 +109,76 @@ void PlayerAttributes::LoadAttributesFromDB(int charID)
 	this->sPlayerAttribute.wBaseEpBattleRegen = 14;
 	this->sPlayerAttribute.wLastEpBattleRegen = 15;
 	//Rp incress rate
-	this->sPlayerAttribute.wBaseRpRegen = 0;
-	this->sPlayerAttribute.wLastRpRegen = 0;
+	this->sPlayerAttribute.wBaseRpRegen = 1;
+	this->sPlayerAttribute.wLastRpRegen = 1;
 	//RP diminution
-	this->sPlayerAttribute.wLastRpDimimutionRate = 0;
+	this->sPlayerAttribute.wLastRpDimimutionRate = 1;
 	//Curse Sucess Rate
-	this->sPlayerAttribute.wBaseCurseSuccessRate = 0;
-	this->sPlayerAttribute.wLastCurseSuccessRate = 0;
+	this->sPlayerAttribute.wBaseCurseSuccessRate = 1;
+	this->sPlayerAttribute.wLastCurseSuccessRate = 1;
 	//Curse Tolerance Rate
-	this->sPlayerAttribute.wBaseCurseToleranceRate = 0;
-	this->sPlayerAttribute.wLastCurseToleranceRate = 0;
+	this->sPlayerAttribute.wBaseCurseToleranceRate = 1;
+	this->sPlayerAttribute.wLastCurseToleranceRate = 1;
 	//Nao sei
-	this->sPlayerAttribute.fCastingTimeChangePercent = 0;
-	this->sPlayerAttribute.fCoolTimeChangePercent = 0;
-	this->sPlayerAttribute.fKeepTimeChangePercent = 0;
-	this->sPlayerAttribute.fDotValueChangePercent = 0;
-	this->sPlayerAttribute.fDotTimeChangeAbsolute = 0;
-	this->sPlayerAttribute.fRequiredEpChangePercent = 0;
+	this->sPlayerAttribute.fCastingTimeChangePercent = 1.0f;
+	this->sPlayerAttribute.fCoolTimeChangePercent = 1.0f;
+	this->sPlayerAttribute.fKeepTimeChangePercent = 1.0f;
+	this->sPlayerAttribute.fDotValueChangePercent = 1.0f;
+	this->sPlayerAttribute.fDotTimeChangeAbsolute = 1.0f;
+	this->sPlayerAttribute.fRequiredEpChangePercent = 1.0f;
 	//Atribute Ofense/Defese
-	this->sPlayerAttribute.fHonestOffence = 0;//nao
-	this->sPlayerAttribute.fHonestDefence = 0;//nao
-	this->sPlayerAttribute.fStrangeOffence = 0;//nao
-	this->sPlayerAttribute.fStrangeDefence = 0;//nao
-	this->sPlayerAttribute.fWildOffence = 0;//nao
-	this->sPlayerAttribute.fWildDefence = 0;//nao
-	this->sPlayerAttribute.fEleganceOffence = 0;//nao
-	this->sPlayerAttribute.fEleganceDefence = 0;//nao
-	this->sPlayerAttribute.fFunnyOffence = 0;//nao
-	this->sPlayerAttribute.fFunnyDefence = 0;//nao
+	this->sPlayerAttribute.fHonestOffence = 1.0f;
+	this->sPlayerAttribute.fHonestDefence = 1.0f;
+	this->sPlayerAttribute.fStrangeOffence = 1.0f;
+	this->sPlayerAttribute.fStrangeDefence = 1.0f;
+	this->sPlayerAttribute.fWildOffence = 1.0f;
+	this->sPlayerAttribute.fWildDefence = 1.0f;
+	this->sPlayerAttribute.fEleganceOffence = 1.0f;
+	this->sPlayerAttribute.fEleganceDefence = 1.0f;
+	this->sPlayerAttribute.fFunnyOffence = 1.0f;
+	this->sPlayerAttribute.fFunnyDefence = 1.0f;
 
-	this->sPlayerAttribute.wParalyzeToleranceRate = 0;//nao
-	this->sPlayerAttribute.wTerrorToleranceRate = 0;//nao
-	this->sPlayerAttribute.wConfuseToleranceRate = 0;//nao
-	this->sPlayerAttribute.wStoneToleranceRate = 0;//nao
-	this->sPlayerAttribute.wCandyToleranceRate = 0;//nao
-	this->sPlayerAttribute.fParalyzeKeepTimeDown = 0;//nao
-	this->sPlayerAttribute.fTerrorKeepTimeDown = 0;//nao
-	this->sPlayerAttribute.fConfuseKeepTimeDown = 0;//nao
-	this->sPlayerAttribute.fStoneKeepTimeDown = 0;//nao
-	this->sPlayerAttribute.fCandyKeepTimeDown = 0;//nao
-	this->sPlayerAttribute.fBleedingKeepTimeDown = 0;//nao
-	this->sPlayerAttribute.fPoisonKeepTimeDown = 0;
-	this->sPlayerAttribute.fStomachacheKeepTimeDown = 0;
-	this->sPlayerAttribute.fCriticalBlockSuccessRate = 0;
-	this->sPlayerAttribute.wGuardRate = 0;
-	this->sPlayerAttribute.fSkillDamageBlockModeSuccessRate = 0;
-	this->sPlayerAttribute.fCurseBlockModeSuccessRate = 0;
-	this->sPlayerAttribute.fKnockdownBlockModeSuccessRate = 0;
+	this->sPlayerAttribute.wParalyzeToleranceRate = 1;
+	this->sPlayerAttribute.wTerrorToleranceRate = 1;
+	this->sPlayerAttribute.wConfuseToleranceRate = 1;
+	this->sPlayerAttribute.wStoneToleranceRate = 1;
+	this->sPlayerAttribute.wCandyToleranceRate = 1;
+	this->sPlayerAttribute.fParalyzeKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fTerrorKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fConfuseKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fStoneKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fCandyKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fBleedingKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fPoisonKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fStomachacheKeepTimeDown = 1.0f;
+	this->sPlayerAttribute.fCriticalBlockSuccessRate = 1.0f;
+	this->sPlayerAttribute.wGuardRate = 1;
+	this->sPlayerAttribute.fSkillDamageBlockModeSuccessRate = 1.0f;
+	this->sPlayerAttribute.fCurseBlockModeSuccessRate = 1.0f;
+	this->sPlayerAttribute.fKnockdownBlockModeSuccessRate = 1.0f;
 
-	this->sPlayerAttribute.fHtbBlockModeSuccessRate = 0; // Bleeding defense TW
-	this->sPlayerAttribute.fSitDownLpRegenBonusRate = 0; // posion defense tw
-	this->sPlayerAttribute.fSitDownEpRegenBonusRate = 0; // abdominal pain defense
-	this->sPlayerAttribute.fPhysicalCriticalDamageBonusRate = 0;//Burn Defense
+	this->sPlayerAttribute.fHtbBlockModeSuccessRate = 1.0f; // Bleeding defense TW
+	this->sPlayerAttribute.fSitDownLpRegenBonusRate = 1.0f; // posion defense tw
+	this->sPlayerAttribute.fSitDownEpRegenBonusRate = 1.0f; // abdominal pain defense
+	this->sPlayerAttribute.fPhysicalCriticalDamageBonusRate = 1.0f; //Burn Defense
 
 	this->sPlayerAttribute.fEnergyCriticalDamageBonusRate = 1;
 	this->sPlayerAttribute.fItemUpgradeBonusRate = 2;
 	this->sPlayerAttribute.fItemUpgradeBreakBonusRate = 3;
-	this->sPlayerAttribute.fBaseAirDash2Speed = 30.0f;//Dash Fly2 TW
-	this->sPlayerAttribute.fLastAirDash2Speed = 30.0f;//Dash Fly2 TW
-	this->sPlayerAttribute.fBaseAirDashSpeed = 20.0f;//Dash Fly TW
-	this->sPlayerAttribute.fLastAirDashSpeed = 20.0f;//Dash Fly TW
+	this->sPlayerAttribute.fBaseAirDash2Speed = 50.0f;//Dash Fly2 TW
+	this->sPlayerAttribute.fLastAirDash2Speed = 50.0f;//Dash Fly2 TW
+	this->sPlayerAttribute.fBaseAirDashSpeed = 30.0f;//Dash Fly TW
+	this->sPlayerAttribute.fLastAirDashSpeed = 30.0f;//Dash Fly TW
 	this->sPlayerAttribute.fBaseRunSpeed = 30;//Base Run TW
 	this->sPlayerAttribute.fLastAirSpeed = 15;//LastAir Speed TW
-	this->sPlayerAttribute.wLastMaxAp = 450000;// db->getInt("LastMaxAp");//Max AP
-	this->sPlayerAttribute.wBaseMaxAp = 450000;// db->getInt("BaseMaxAp");//Base Max Ap
-	this->sPlayerAttribute.wBaseApBattleRegen = 5000;//Regen In Battle AP TW
-	this->sPlayerAttribute.wLastApBattleRegen = 5000;//LAst Regen in Battle Ap TW
-	this->sPlayerAttribute.wBaseApRegen = 5000;//Base Ap Regen TW
-	this->sPlayerAttribute.wLastApRegen = 5000;//Base While Sitting Regen AP TW
-	this->sPlayerAttribute.wBaseApSitdownRegen = 5000;//AP Regen TW
-	this->sPlayerAttribute.wLastApSitdownRegen = 5000;//AP Regen TW
+	this->sPlayerAttribute.wLastMaxAp = 0x1B17A;// db->getInt("LastMaxAp");//Max AP
+	this->sPlayerAttribute.wBaseMaxAp = 0x1B17A;// db->getInt("BaseMaxAp");//Base Max Ap
+	this->sPlayerAttribute.wBaseApBattleRegen = 5;//Regen In Battle AP TW
+	this->sPlayerAttribute.wLastApBattleRegen = 5;//LAst Regen in Battle Ap TW
+	this->sPlayerAttribute.wBaseApRegen = 5;//Base Ap Regen TW
+	this->sPlayerAttribute.wLastApRegen = 5;//Base While Sitting Regen AP TW
+	this->sPlayerAttribute.wBaseApSitdownRegen = 5;//AP Regen TW
+	this->sPlayerAttribute.wLastApSitdownRegen = 5;//AP Regen TW
 
 	
 }
